fd2d2fb0927ddf7ee6b34f52f0a5944dcaca5328
Pavlo Kotsan <paul@Pavlos-MacBook-Pro.local>
2024-12-26 19:56:25 -0500

    make build

---
+++ index.e92bbe07.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=document.querySelector(".wall"),o=e.offsetWidth,s=e.offsetHeight,l=t.offsetWidth,i=t.offsetHeight;e.style.position="relative",t.style.position="absolute",t.style.top="".concat((s-i)/2,"px"),t.style.left="".concat((o-l)/2,"px");
+//# sourceMappingURL=index.e92bbe07.js.map
